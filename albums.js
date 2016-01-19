2a5aede1d4684da7940dd75d3521b9e21125a4fa
Zachary Lewis <zachwlewis@gmail.com>
2016-01-19 15:10:50 -0600

    Switched to a data-driven page; added player UI.

---
+++ albums.js
@@ -0,0 +1,45 @@
+// {title: "", src:"", length: ""},
+
+var albums = [
+  {title: "Tryah", year: "2016", tracks: [ 
+    {title: "Tryah", src:"https://s3.amazonaws.com/wearedevelopers/Tryah/Tryah.mp3", length: "20:27"},
+  ]},
+
+  {title: "Clothes For Christmas", year: "2014", tracks: [ 
+    {title: "Clothes For Christmas", src:"https://s3.amazonaws.com/wearedevelopers/Clothes+For+Christmas/Clothes+For+Christmas.mp3", length: "6:08"},
+    {title: "Every Season Is The Same", src:"https://s3.amazonaws.com/wearedevelopers/Clothes+For+Christmas/Every+Season+Is+The+Same.mp3", length: "6:19"},
+    {title: "Carol Of The Bells", src:"https://s3.amazonaws.com/wearedevelopers/Clothes+For+Christmas/Carol+Of+The+Bells.mp3", length: "7:48"},
+  ]},
+
+  {title: "Dreams", year: "2014", tracks: [ 
+    {title: "You Can Breathe In Space", src:"https://s3.amazonaws.com/wearedevelopers/Dreams/You+Can+Breathe+In+Space.mp3", length: "9:27"},
+    {title: "Elysian Fields At Dusk", src:"https://s3.amazonaws.com/wearedevelopers/Dreams/Elysian+Fields+At+Dusk.mp3", length: "14:05"},
+    {title: "Unyeilding", src:"https://s3.amazonaws.com/wearedevelopers/Unyeilding.mp3", length: "5:07"},
+    {title: "Wandering", src:"https://s3.amazonaws.com/wearedevelopers/Wandering.mp3", length: "6:07"},
+    {title: "Widow's Walk", src:"https://s3.amazonaws.com/wearedevelopers/Widow%27s+Walk.mp3", length: "20:44"},
+    {title: "Voyager", src:"https://s3.amazonaws.com/wearedevelopers/Voyager.mp3", length: "7:06"},
+  ]},
+
+  {title: "Figures", year: "2015", tracks: [ 
+    {title: "Figure G", src:"https://s3.amazonaws.com/wearedevelopers/Figure+G.mp3", length: "8:11"},
+  ]},
+
+  {title: "Complete Sessions", year: "", tracks: [ 
+    {title: "10 December 2015", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(10+December+2015).mp3", length: "1:23:11"},
+    {title: "12 November 2015", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(12+November+2015).mp3", length: "50:01"},
+    {title: "3 September 2015", src:"https://s3.amazonaws.com/wearedevelopers/05+Jacob%27s+Twenty+Seventh+Birthday.mp3", length: "1:49:27"},
+    {title: "Heartbeat (20 August 2015)", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(20+August+2015).mp3", length: "1:17:16"},
+    {title: "6 August 2015", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(6+August+2015).mp3", length: "1:02:40"},
+    {title: "24 June 2015", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(24+June+2015).mp3", length: "57:15"},
+    {title: "13 May 2015", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(13+May+2015).mp3", length: "1:20:51"},
+  ]},
+
+  {title: "Other", year: "", tracks: [ 
+    {title: "Awaken", src:"https://s3.amazonaws.com/wearedevelopers/Awaken.mp3", length: "4:49"},
+    {title: "Mountain Children", src:"https://s3.amazonaws.com/wearedevelopers/Dreams/Mountain+Children.mp3", length: "2:03"},
+    {title: "Children Dressed As Dead Nobility", src:"https://s3.amazonaws.com/wearedevelopers/Children+Dressed+As+Dead+Nobility.mp3", length: "2:22"},
+    {title: "Brooke", src:"https://s3.amazonaws.com/wearedevelopers/Brooke.mp3", length: "2:55"},
+    {title: "Dyson", src:"https://s3.amazonaws.com/wearedevelopers/Dyson.mp3", length: "10:12"},
+  ]},
+
+];
