f593b1a13214a54b9ab09fb2ac298226a2cdae12
Zachary Lewis <zachwlewis@gmail.com>
2018-08-22 12:33:58 -0700

    Update albums.js

--- albums.js
+++ albums.js
@@ -1,19 +1,29 @@
 // {title: "", src:"", length: ""},
 
 var albums = [
-  {title: "Christmas 2016", year: "2016", tracks: [
-      {title: "Buy It For Yourself", src:"https://s3.amazonaws.com/wearedevelopers/Christmas+2016/Buy+It+For+Yourself.mp3", length: "5:31"},
-      {title: "I'm Santa", src:"https://s3.amazonaws.com/wearedevelopers/Christmas+2016/I%27m+Santa.mp3", length: "6:04"},
-    ]},
-  
-  {title: "Tryah", year: "2016", tracks: [ 
+  {title: "Chill Ambient Compilation (Recommended Listening)", year: "", tracks: [
     {title: "Tryah", src:"https://s3.amazonaws.com/wearedevelopers/Tryah/Tryah.mp3", length: "20:27"},
+    {title: "You Can Breathe In Space", src:"https://s3.amazonaws.com/wearedevelopers/Dreams/You+Can+Breathe+In+Space.mp3", length: "9:27"},
+    {title: "Elysian Fields At Dusk", src:"https://s3.amazonaws.com/wearedevelopers/Dreams/Elysian+Fields+At+Dusk.mp3", length: "14:05"},
+    {title: "Unyeilding", src:"https://s3.amazonaws.com/wearedevelopers/Unyeilding.mp3", length: "5:07"},
+    {title: "Wandering", src:"https://s3.amazonaws.com/wearedevelopers/Wandering.mp3", length: "6:07"},
+    {title: "Widow's Walk", src:"https://s3.amazonaws.com/wearedevelopers/Widow%27s+Walk.mp3", length: "20:44"},
+    {title: "Voyager", src:"https://s3.amazonaws.com/wearedevelopers/Voyager.mp3", length: "7:06"},
   ]},
-
-  {title: "Clothes For Christmas", year: "2014", tracks: [ 
-    {title: "Clothes For Christmas", src:"https://s3.amazonaws.com/wearedevelopers/Clothes+For+Christmas/Clothes+For+Christmas.mp3", length: "6:08"},
-    {title: "Every Season Is The Same", src:"https://s3.amazonaws.com/wearedevelopers/Clothes+For+Christmas/Every+Season+Is+The+Same.mp3", length: "6:19"},
-    {title: "Carol Of The Bells", src:"https://s3.amazonaws.com/wearedevelopers/Clothes+For+Christmas/Carol+Of+The+Bells.mp3", length: "7:48"},
+  
+  {title: "Experimental Bullshit (For True Fans)", year: "", tracks: [
+    {title: "Figure G", src:"https://s3.amazonaws.com/wearedevelopers/Figure+G.mp3", length: "8:11"},
+    {title: "Under The Sea", src:"https://s3.amazonaws.com/wearedevelopers/Under+The+Sea.mp3", length: "23:18"},
+    {title: "Awaken", src:"https://s3.amazonaws.com/wearedevelopers/Awaken.mp3", length: "4:49"},
+    {title: "Mountain Children", src:"https://s3.amazonaws.com/wearedevelopers/Dreams/Mountain+Children.mp3", length: "2:03"},
+    {title: "Children Dressed As Dead Nobility", src:"https://s3.amazonaws.com/wearedevelopers/Children+Dressed+As+Dead+Nobility.mp3", length: "2:22"},
+    {title: "Brooke", src:"https://s3.amazonaws.com/wearedevelopers/Brooke.mp3", length: "2:55"},
+    {title: "Dyson", src:"https://s3.amazonaws.com/wearedevelopers/Dyson.mp3", length: "10:12"},
+    {title: "Heartbeat (20 August 2015)", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(20+August+2015).mp3", length: "1:17:16"},
+  ]},
+  
+  {title: "Tryah (Single)", year: "2016", tracks: [ 
+    {title: "Tryah", src:"https://s3.amazonaws.com/wearedevelopers/Tryah/Tryah.mp3", length: "20:27"},
   ]},
 
   {title: "Dreams", year: "2014", tracks: [ 
@@ -23,13 +33,14 @@ var albums = [
     {title: "Wandering", src:"https://s3.amazonaws.com/wearedevelopers/Wandering.mp3", length: "6:07"},
     {title: "Widow's Walk", src:"https://s3.amazonaws.com/wearedevelopers/Widow%27s+Walk.mp3", length: "20:44"},
     {title: "Voyager", src:"https://s3.amazonaws.com/wearedevelopers/Voyager.mp3", length: "7:06"},
+    {title: "Tryah", src:"https://s3.amazonaws.com/wearedevelopers/Tryah/Tryah.mp3", length: "20:27"},
   ]},
 
   {title: "Figures", year: "2015", tracks: [ 
     {title: "Figure G", src:"https://s3.amazonaws.com/wearedevelopers/Figure+G.mp3", length: "8:11"},
   ]},
 
-  {title: "Complete Sessions", year: "", tracks: [ 
+  {title: "Complete Sessions (Uncut, unmixed complete recording sessions)", year: "", tracks: [ 
     {title: "10 December 2015", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(10+December+2015).mp3", length: "1:23:11"},
     {title: "12 November 2015", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(12+November+2015).mp3", length: "50:01"},
     {title: "3 September 2015", src:"https://s3.amazonaws.com/wearedevelopers/05+Jacob%27s+Twenty+Seventh+Birthday.mp3", length: "1:49:27"},
@@ -38,14 +49,16 @@ var albums = [
     {title: "24 June 2015", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(24+June+2015).mp3", length: "57:15"},
     {title: "13 May 2015", src:"https://s3.amazonaws.com/wearedevelopers/Complete+Session+(13+May+2015).mp3", length: "1:20:51"},
   ]},
-
-  {title: "Other", year: "", tracks: [ 
-    {title: "Under The Sea", src:"https://s3.amazonaws.com/wearedevelopers/Under+The+Sea.mp3", length: "23:18"},
-    {title: "Awaken", src:"https://s3.amazonaws.com/wearedevelopers/Awaken.mp3", length: "4:49"},
-    {title: "Mountain Children", src:"https://s3.amazonaws.com/wearedevelopers/Dreams/Mountain+Children.mp3", length: "2:03"},
-    {title: "Children Dressed As Dead Nobility", src:"https://s3.amazonaws.com/wearedevelopers/Children+Dressed+As+Dead+Nobility.mp3", length: "2:22"},
-    {title: "Brooke", src:"https://s3.amazonaws.com/wearedevelopers/Brooke.mp3", length: "2:55"},
-    {title: "Dyson", src:"https://s3.amazonaws.com/wearedevelopers/Dyson.mp3", length: "10:12"},
+  
+  {title: "Christmas 2016", year: "2016", tracks: [
+      {title: "Buy It For Yourself", src:"https://s3.amazonaws.com/wearedevelopers/Christmas+2016/Buy+It+For+Yourself.mp3", length: "5:31"},
+      {title: "I'm Santa", src:"https://s3.amazonaws.com/wearedevelopers/Christmas+2016/I%27m+Santa.mp3", length: "6:04"},
+    ]},
+  
+  {title: "Clothes For Christmas", year: "2014", tracks: [ 
+    {title: "Clothes For Christmas", src:"https://s3.amazonaws.com/wearedevelopers/Clothes+For+Christmas/Clothes+For+Christmas.mp3", length: "6:08"},
+    {title: "Every Season Is The Same", src:"https://s3.amazonaws.com/wearedevelopers/Clothes+For+Christmas/Every+Season+Is+The+Same.mp3", length: "6:19"},
+    {title: "Carol Of The Bells", src:"https://s3.amazonaws.com/wearedevelopers/Clothes+For+Christmas/Carol+Of+The+Bells.mp3", length: "7:48"},
   ]},
 
 ];
